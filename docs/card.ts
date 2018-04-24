de363e46f71516ed746d45ebf2d0ceb8719991ac
Wang <WANGMH@TDBFG.COM>
2018-04-23 21:16:39 -0400

    update docs

--- docs/card.ts
+++ docs/card.ts
@@ -1,10 +1,7 @@
-/**
- * Created by jetyue on 2018-03-10.
- */
 export class Card {
   id: number;
   name: string;
   description: string;
   annualFee: number;
-  interest: number;
+  interest: number
 }
